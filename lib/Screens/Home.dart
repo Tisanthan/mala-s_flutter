0fd7212e1743717663070218dd83b1cb32f5c988
tisanthan <tisanthan12@gmail.com>
2020-11-21 12:46:48 +0530

    first Commit

---
+++ lib/Screens/Home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              width: 200,
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/fruid.jpg"),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(22.0),
+              ),
+              elevation: 5.0,
+              minWidth: 200.0,
+              height: 45,
+              color: Colors.blue,
+              child: new Text('History',
+                  style: new TextStyle(fontSize: 16.0, color: Colors.white)),
+              onPressed: () {},
+            ),
+            const SizedBox(height: 30),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(22.0),
+              ),
+              elevation: 5.0,
+              minWidth: 200.0,
+              height: 45,
+              color: Colors.blue,
+              child: new Text('Upload image',
+                  style: new TextStyle(fontSize: 16.0, color: Colors.white)),
+              onPressed: () => Navigator.pushNamed(
+                context,
+                '/second',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+import 'Screens/Home.dart';
+import 'Screens/Second_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => Home(),
+        '/second': (context) => SecondScreen(),
+      },
+    );
+  }
+}
